641a6948977f1e347ce76d426a33aaa5a4d68837
Danilo Cordeiro <danilocordeiro.dev@gmail.com>
2019-05-12 17:12:14 -0300

    first commit

---
+++ course/src/main/java/academy/devdojo/course/model/Course.java
@@ -0,0 +1,25 @@
+package academy.devdojo.course.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String title;
+}
