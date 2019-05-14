f5f50576ea66a2ea6291dda8ea56359555a766d3
Danilo Cordeiro <danilocordeiro.dev@gmail.com>
2019-05-14 12:32:15 -0300

    Finish authorization filter and add swagger doc

--- course/src/main/java/academy/devdojo/course/endpoint/controller/CourseController.java
+++ course/src/main/java/academy/devdojo/course/endpoint/controller/CourseController.java
@@ -3,6 +3,8 @@ package academy.devdojo.course.endpoint.controller;
 import academy.devdojo.core.model.Course;
 import academy.devdojo.course.endpoint.service.CourseService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/admin/course")
 @Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Api(value = "Endpoints to manage course")
 public class CourseController {
     private final CourseService courseService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "Lis all available courses", response = Course[].class)
     public ResponseEntity<Iterable<Course>> list(Pageable pageable) {
         return new ResponseEntity<>(courseService.list(pageable), HttpStatus.OK);
     }
---
+++ course/src/main/java/academy/devdojo/course/security/config/SecurityCredentialsConfig.java
@@ -0,0 +1,32 @@
+package academy.devdojo.course.security.config;
+
+
+import academy.devdojo.core.property.JwtConfiguration;
+import academy.devdojo.security.config.SecurityTokenConfig;
+import academy.devdojo.security.filter.JwtTokenAuthorizationFilter;
+import academy.devdojo.security.token.converter.TokenConverter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@EnableWebSecurity
+public class SecurityCredentialsConfig extends SecurityTokenConfig {
+
+    private final TokenConverter tokenConverter;
+
+    public SecurityCredentialsConfig(JwtConfiguration jwtConfiguration, TokenConverter tokenConverter) {
+        super(jwtConfiguration);
+        this.tokenConverter = tokenConverter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterAfter(new JwtTokenAuthorizationFilter(jwtConfiguration, tokenConverter), UsernamePasswordAuthenticationFilter.class);
+        super.configure(http);
+
+    }
+
+
+}
